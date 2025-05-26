6c23d3665f72d08b751df1e460362185ac22ef10
Anitha_kolla <153736044+yashna2457@users.noreply.github.com>
2025-05-26 22:08:08 +0530

    Add files via upload

---
+++ SQL/Sessions.sql
@@ -0,0 +1,16 @@
+USE EventPortal;
+CREATE TABLE Sessions (
+    session_id INT PRIMARY KEY AUTO_INCREMENT,
+    event_id INT,
+    title VARCHAR(200) NOT NULL,
+    speaker_name VARCHAR(100) NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
+INSERT INTO Sessions (session_id, event_id, title, speaker_name, start_time, end_time) VALUES
+(1, 1, 'Opening Keynote', 'Dr. Tech', '2025-06-10 10:00:00', '2025-06-10 11:00:00'),
+(2, 1, 'Future of Web Dev', 'Alice Johnson', '2025-06-10 11:15:00', '2025-06-10 12:30:00'),
+(3, 2, 'AI in Healthcare', 'Charlie Lee', '2025-05-15 09:30:00', '2025-05-15 11:00:00'),
+(4, 3, 'Intro to HTML5', 'Bob Smith', '2025-07-01 10:00:00', '2025-07-01 12:00:00');
+select *From Sessions;
