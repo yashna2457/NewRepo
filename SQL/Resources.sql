bc12bb1a1e232061c189b99b0b456b548e41748a
Anitha_kolla <153736044+yashna2457@users.noreply.github.com>
2025-05-26 22:15:06 +0530

    Add files via upload

---
+++ SQL/Resources.sql
@@ -0,0 +1,14 @@
+USE EventPortal;
+CREATE TABLE Resources (
+    resource_id INT PRIMARY KEY AUTO_INCREMENT,
+    event_id INT,
+    resource_type ENUM('pdf', 'image', 'link'),
+    resource_url VARCHAR(255) NOT NULL,
+    uploaded_at DATETIME NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
+INSERT INTO Resources (resource_id, event_id, resource_type, resource_url, uploaded_at) VALUES
+(1, 1, 'pdf', 'https://portal.com/resources/tech_meetup_agenda.pdf', '2025-05-01 10:00:00'),
+(2, 2, 'image', 'https://portal.com/resources/ai_poster.jpg', '2025-04-20 09:00:00'),
+(3, 3, 'link', 'https://portal.com/resources/html5_docs', '2025-06-25 15:00:00');
+select *From Resources;
