6843355cbb45ab91d787cd40b00d4444aeabf208
Anitha_kolla <153736044+yashna2457@users.noreply.github.com>
2025-05-26 22:07:35 +0530

    Add files via upload

---
+++ SQL/Events.sql
@@ -0,0 +1,17 @@
+USE EventPortal;
+CREATE TABLE Events (
+    event_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    city VARCHAR(100) NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    status ENUM('upcoming', 'completed', 'cancelled'),
+    organizer_id INT,
+    FOREIGN KEY (organizer_id) REFERENCES Users(user_id)
+);
+INSERT INTO Events (event_id, title, description, city, start_date, end_date, status, organizer_id) VALUES
+(1, 'Tech Innovators Meetup', 'A meetup for tech enthusiasts.', 'New York', '2025-06-10 10:00:00', '2025-06-10 16:00:00', 'upcoming', 1),
+(2, 'AI & ML Conference', 'Conference on AI and ML advancements.', 'Chicago', '2025-05-15 09:00:00', '2025-05-15 17:00:00', 'completed', 3),
+(3, 'Frontend Development Bootcamp', 'Hands-on training on frontend tech.', 'Los Angeles', '2025-07-01 10:00:00', '2025-07-03 16:00:00', 'upcoming', 2);
+select *From Events;
