0a9dad4916f7b402e1ce5c316618adcfcbfc3702
Anitha_kolla <153736044+yashna2457@users.noreply.github.com>
2025-05-26 22:09:10 +0530

    Add files via upload

---
+++ SQL/Registrations.sql
@@ -0,0 +1,16 @@
+USE EventPortal;
+CREATE TABLE Registrations (
+    registration_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    event_id INT,
+    registration_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
+INSERT INTO Registrations (registration_id, user_id, event_id, registration_date) VALUES
+(1, 1, 1, '2025-05-01'),
+(2, 2, 1, '2025-05-02'),
+(3, 3, 2, '2025-04-30'),
+(4, 4, 2, '2025-04-28'),
+(5, 5, 3, '2025-06-15');
+select *From Registrations;
