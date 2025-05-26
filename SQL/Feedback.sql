d07abc092a8c324be34ed527e7416b811213500c
Anitha_kolla <153736044+yashna2457@users.noreply.github.com>
2025-05-26 22:11:14 +0530

    Add files via upload

---
+++ SQL/Feedback.sql
@@ -0,0 +1,16 @@
+USE EventPortal;
+CREATE TABLE Feedback (
+    feedback_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    event_id INT,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
+    feedback_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
+INSERT INTO Feedback (feedback_id, user_id, event_id, rating, comments, feedback_date) VALUES
+(1, 3, 2, 4, 'Great insights!', '2025-05-16'),
+(2, 4, 2, 5, 'Very informative.', '2025-05-16'),
+(3, 2, 1, 3, 'Could be better.', '2025-06-11');
+select *From Feedback;
